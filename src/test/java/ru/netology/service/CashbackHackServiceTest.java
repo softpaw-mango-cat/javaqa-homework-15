3373053505303e6c713e35efac2a0d02c8d72c69
Katja <katerina-balakireva@yandex.ru>
2026-01-08 20:04:07 +0300

    added tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CashbackHackServiceTest {
+
+     private CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturnCorrectAmountWhenBelowBoundary() {
+      int actual = service.remain(999);
+      int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCorrectAmountWhenEqualsBoundary() {
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCorrectAmountWhenAboveBoundary() {
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
